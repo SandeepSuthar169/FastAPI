f3d9f5ae0f356ed6a50798db80d19d371bad12cb
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-06-02 18:31:05 +0530

    first commit

---
+++ 2/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as file:
+      data = json.load(file)
+      
+      return data
+
+@app.get("/")
+def hello():
+    return {'message':' get page'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
